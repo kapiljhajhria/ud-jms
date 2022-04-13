7e9b330db59c3c97b6aa910b519e4169b795f54f
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-04-13 23:36:10 +0530

    Configure app to connect to local broker running in docker
    - comment out server definition from main class
    - configuration was set explicitly. Doesn't need to set it up as spring-boot will automatically configure the app when it notices the class associated with
    - you can comment out the @Component from listener class, restart the app and then check queues in the activeMQ console

--- src/main/java/com/jhajhria/jms/JmsApplication.java
+++ src/main/java/com/jhajhria/jms/JmsApplication.java
@@ -1,8 +1,5 @@
 package com.jhajhria.jms;
 
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,14 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JmsApplication {
 
     public static void main(String[] args) throws Exception {
+//       below configuration was set explicitly. Doesn't need to set it up as spring-boot will automatically configure the app when it notices the class associated with
 
-        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
-                .setPersistenceEnabled(false)
-                .setJournalDirectory("target/data/journal")
-                .setSecurityEnabled(false)
-                .addAcceptorConfiguration("invm", "vm://0"));
-
-        server.start();
+//        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+//                .setPersistenceEnabled(false)
+//                .setJournalDirectory("target/data/journal")
+//                .setSecurityEnabled(false)
+//                .addAcceptorConfiguration("invm", "vm://0"));
+//
+//        server.start();
         SpringApplication.run(JmsApplication.class, args);
     }
 
