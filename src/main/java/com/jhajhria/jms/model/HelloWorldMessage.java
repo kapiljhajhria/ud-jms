c9a7058ad97d2502fd4eda69a9a204a0565d801c
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-04-13 14:19:07 +0530

    Java Messaging Object POJO

---
+++ src/main/java/com/jhajhria/jms/model/HelloWorldMessage.java
@@ -0,0 +1,22 @@
+package com.jhajhria.jms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    private final long serialVersionUID = -9143763644821309717L;
+
+
+    private UUID id;
+    private String message;
+}
